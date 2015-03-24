f7693752e24688720672699eba9d65fd8c1b2310
k2works <kakimomokuri@gmail.com>
2015-03-24 20:13:31 +0900

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'php-env'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures php-env'
+long_description 'Installs/Configures php-env'
+version          '0.1.0'
+
